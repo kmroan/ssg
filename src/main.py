dee8b178430ef21848734d863baeb56067fee943
Keith Roan <11397454+kmroan@users.noreply.github.com>
2026-02-21 19:55:30 -0500

    ch2l1

--- src/main.py
+++ src/main.py
@@ -1,1 +1,8 @@
-print("hello world")
+from textnode import TextNode, TextType
+def main():
+    test = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
+    print(test)
+
+
+if __name__ == "__main__":
+    main()
