44d1b0f9b5b52e32bc2cab53fde450260681635a
Keith Roan <11397454+kmroan@users.noreply.github.com>
2026-02-25 14:17:32 -0500

    ch2/3

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -6,3 +6,17 @@ class HTMLNode():
         self.value = value
         self.children = children
         self.props = props
+        
+    def to_html(self):
+        pass
+    
+    def props_to_html(self):
+        if self.props is None:
+            return None
+        res = []
+        for p in self.props:
+            res.append(f"{p}=\"{self.props[p]}\"")
+        return " ".join(res)
+
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
---
+++ src/test_htmlnode.py
@@ -0,0 +1,22 @@
+import unittest
+
+from htmlnode import HTMLNode
+
+
+class TestTextNode(unittest.TestCase):
+    def test_props_to_html_single(self):
+        node = HTMLNode("a","test",None,{"class":"test"})
+        self.assertEqual(node.props_to_html(), "class=\"test\"")
+
+    def test_props_to_html_mutli(self):
+        node = HTMLNode("a","test",None,{"href":"https://boot.dev","class":"test","id":"test1"})
+        self.assertEqual(node.props_to_html(), "href=\"https://boot.dev\" class=\"test\" id=\"test1\"")
+
+    def test_props_to_html_none(self):
+        node = HTMLNode("a","test",None,None)
+        self.assertEqual(node.props_to_html(), None)
+
+
+
+if __name__ == "__main__":
+    unittest.main()
